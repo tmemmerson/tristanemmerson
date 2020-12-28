fca4c48ee84dd2f912e6dbbaf2732fd3c0fb33fb
Tristan Emmerson <tristan@stickerslug.com>
2020-12-28 15:48:07 -0800

    create fade animation for first slide

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+})
+
+
+
+
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+  });
+})
