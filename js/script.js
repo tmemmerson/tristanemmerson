6c98a650c74c3c3acffc61b878b67b96c6a913fa
Tristan Emmerson <tristan@stickerslug.com>
2020-12-30 21:04:24 -0800

    apply jquery class removal assignment on scroll params

--- js/script.js
+++ js/script.js
@@ -97,5 +97,18 @@ $(".items").isotope({
 		return false;
 	})
 
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+$(window).on("scroll", stickyNavigation);
 
+function stickyNavigation() {
+	var body = $("body");
+
+	if($(window).scrollTop() >= navTop) {
+		body.addClass("fixedNav");
+	}
+	else {
+		body.removeClass("fixedNav");
+	}
+}
 });
