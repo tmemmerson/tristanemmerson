1f60ca08f54ed3f8a4b25a01b78398aee3404381
Tristan Emmerson <tristan@stickerslug.com>
2020-12-30 21:15:30 -0800

    debug sticky pickup on navbar

--- js/script.js
+++ js/script.js
@@ -80,38 +80,53 @@ $(".items").isotope({
 	}
 });
 
+$("#filters a").click(function() {
 
-	$("#filters a").click(function(){
-		$("#filters .current").removeClass("current");
-		$(this).addClass("current");
+	$("#filters .current").removeClass("current");
+	$(this).addClass("current");
 
-		var selector = $(this).attr("data-filter");
-		$(".items").isotope({
-			filter: selector,
-			animationOptions: {
-				duration: 1500,
-				easing: 'linear',
-				queue: false
-			}
-		})
-		return false;
-	})
+	var selector = $(this).attr("data-filter");
 
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
-
-	$(window).on("scroll", stickyNavigation);
-	function stickyNavigation() {
-		var body = $("body");
-		if($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px");
-			body.addClass("fixedNav");
+	$(".items").isotope({
+		filter: selector,
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
 		}
-		else {
-			body.css("padding-top", 0);
-			body.removeClass("fixedNav");
-		}
-	}
+	});
+
+	return false;
+});
+
+$("#navigation li a").click(function(e) {
+	e.preventDefault();
+
+	var targetElement = $(this).attr("href");
+	var targetPosition = $(targetElement).offset().top;
+	$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
 
 });
 
+
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+
+$(window).on("scroll", stickyNavigation);
+
+function stickyNavigation() {
+
+	var body = $("body");
+
+	if($(window).scrollTop() >= navTop) {
+		body.css("padding-top", nav.outerHeight() + "px");
+		body.addClass("fixedNav");
+	}
+	else {
+		body.css("padding-top", 0);
+		body.removeClass("fixedNav");
+	}
+
+}
+
+});
