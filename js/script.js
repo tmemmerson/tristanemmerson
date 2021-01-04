370a5fee3e3b87ce60d358633a94a6bdcad69f73
Tristan Emmerson <tristan@stickerslug.com>
2021-01-04 10:37:51 -0800

    debug portfolio image load overlapping

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ $(window).on("load", function() {
 	$(".loader .inner").fadeOut(500, function() {
 		$(".loader").fadeOut(750);
 	});
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
 
 })
 
@@ -75,14 +83,7 @@ var skillsTopOffset = $(".skillsSection").offset().top;
 	$("[data-fancybox]").fancybox();
 
 
-	$(".items").isotope({
-		filter: '*',
-		animationOptions: {
-			duration: 1500,
-			easing: 'linear',
-			queue: false
-		}
-	});
+
 
 	$("#filters a").click(function() {
 		$("#filters .current").removeClass("current");
